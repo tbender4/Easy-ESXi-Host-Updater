9be4c2d44ba37bdda03c9b6bb65afb18c46500e0
Thomas Bender <tbender4@gmail.com>
2018-04-23 11:49:38 -0400

    Experimenting with parameterized filter

--- list-profiles.sh
+++ list-profiles.sh
@@ -2,9 +2,29 @@
 
 
 echo "Available profiles:"
-command='esxcli software sources profile list --depot=https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml'
+#command='esxcli software sources profile list --depot=https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml'
+command='cat profiles.txt'
 
+filter='/standard/!d' 
+
+if [ $# > 1 ]
+then
+	echo "Usage: $0 filter"
+	echo "-----------------"
+	echo "Filters available: standard | security-only | no-tools | so+nt | all"
+	exit 1
+fi
+
+case "$1" in
+	standard)
+		filter='/standard/!d';;
+	security-only) filter='/s-standard/!d';;
+	so+nt) filter='/s-no-tools/!d';;
+	no-tools) filter="'/s-no-tools/d' -e '/no-tools/!d'";;
+	all) filter="";;
+	*) exit 1;;
+esac
 
 $command | sort -k4 | sed \
-	-e '/standard/!d' \
-	-e 's/VMware.*$//'
+	$filter
+#	-e 's/VMware.*$//'
